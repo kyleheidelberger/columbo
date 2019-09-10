a09dd226ef7c680ebfc9ab3d005c9659f3b10f47
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-10 13:12:44 -0400

    working on models

--- columbo/columboAPI/models.py
+++ columbo/columboAPI/models.py
@@ -4,7 +4,45 @@ from django.db import models
 
 class Episode(models.Model):
     title = models.CharField(max_length=100)
-    season = models.CharField(max_length=2)
+    season = models.PositiveIntegerField
+    air_date = models.DateField
+    director = models.ForeignKey('Director')
+    writer = models.ForeignKey('Writer')
+    season_num = models.PositiveIntegerField
+    series_num
+    runtime = models.DurationField
+    fun_facts = models.TextField
+    ep_image = models.URLField
 
     def __str__(self):
         return self.title
+
+class Season(models.Model):
+    number
+    episodes
+    year_aired
+
+class Character(models.Model):
+    title
+    first_name
+    last_name
+    played_by
+    appears_in
+    fun_facts
+
+class Actor(models.Model):
+    first_name
+    last_name
+    appeared_as
+    appeared_in
+    fun_facts
+
+class Director(models.Model):
+    first_name
+    last_name
+    worked_on
+
+class Writer(models.Model):
+    first_name
+    last_name
+    worked_on
