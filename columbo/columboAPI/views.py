694385de087000fd68cfb5ab8ecf5d956ea275d5
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-10 12:40:39 -0400

    add urls

--- columbo/columboAPI/views.py
+++ columbo/columboAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import EpisodeSerializer
+from .models import Episode
 
 # Create your views here.
+
+class EpisodeViewSet(viewsets.ModelViewSet):
+    queryset = Episode.objects.all().order_by('season')
+    serializer_class = EpisodeSerializer
