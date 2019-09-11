de1a1dd663f6b8479a5b9389899014dc58bfbfb2
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-11 08:52:50 -0400

    api working

--- columbo/columboAPI/models.py
+++ columbo/columboAPI/models.py
@@ -5,39 +5,39 @@ from django.db import models
 
 class Episode(models.Model):
     title = models.CharField(max_length=100)
-    season = models.PositiveIntegerField
-    air_date = models.DateField
-    director = models.ForeignKey('Director', on_delete=models.CASCADE)
-    writer = models.ForeignKey('Writer', on_delete=models.CASCADE)
-    season_num = models.PositiveIntegerField
-    series_num = models.PositiveIntegerField
-    runtime = models.DurationField
-    fun_facts = models.TextField
-    ep_image = models.URLField
+    season = models.PositiveIntegerField(default=00)
+    air_date = models.DateField(null=True, blank=True)
+    director = models.ManyToManyField('Director', default='')
+    writer = models.ManyToManyField('Writer', default='')
+    season_num = models.PositiveIntegerField(default=00)
+    series_num = models.PositiveIntegerField(default=00)
+    runtime = models.DurationField(null=True, blank=True)
+    fun_facts = models.TextField(null=True, blank=True)
+    ep_image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Season(models.Model):
-    number = models.PositiveIntegerField
-    episodes = models.PositiveIntegerField
-    year_aired = models.PositiveIntegerField
+    number = models.PositiveIntegerField(default=00)
+    episodes = models.PositiveIntegerField(default=00)
+    year_aired = models.PositiveIntegerField(default=00)
 
     def __str__(self):
-        return self.number
+        return f'{self.number}'
 
 
 class Character(models.Model):
-    title = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    occupation = models.CharField(max_length=100)
-    murderer = models.BooleanField
-    victim = models.BooleanField
-    played_by = models.ForeignKey('Actor', on_delete=models.CASCADE)
-    appears_in = models.ForeignKey(Episode, on_delete=models.CASCADE)
-    fun_facts = models.TextField
+    title = models.CharField(max_length=100, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    occupation = models.CharField(max_length=100, null=True, blank=True)
+    murderer = models.BooleanField(null=True, blank=True)
+    victim = models.BooleanField(null=True, blank=True)
+    played_by = models.ForeignKey('Actor', on_delete=models.CASCADE, default='')
+    appears_in = models.ForeignKey(Episode, on_delete=models.CASCADE, default='')
+    fun_facts = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -46,7 +46,7 @@ class Character(models.Model):
 class Actor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    fun_facts = models.TextField
+    fun_facts = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
